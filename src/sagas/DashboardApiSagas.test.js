3c8d8aaa66e8bc8895b4382593c639e48f036073
Aleskey Troynikov <atroynikov@yandex.ru>
2018-09-22 01:16:03 +0300

    Try to test

--- src/sagas/DashboardApiSagas.test.js
+++ src/sagas/DashboardApiSagas.test.js
@@ -1,7 +1,19 @@
+import 'babel-polyfill';
+import {put, call, getContext} from 'redux-saga/effects';
 import {
     fetchConfigurationSaga
 } from '@sagas/DashboardApiSagas';
+import {
+    requestFetchConfiguration
+} from '@actions/DashboardApiActions';
 
 describe('DashboardApiSagas', () => {
+    describe('fetchConfigurationSaga', () => {
+        it('Test', () => {
+            const generator = fetchConfigurationSaga();
 
+            (generator.next().value).should.deep.equal(put(requestFetchConfiguration()));
+            (generator.next().value).should.deep.equal(getContext('dashboardApi'));
+        });
+    });
 });
